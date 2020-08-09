7eb6095b6d4281807b48919cccd5ca63266b33c2
Mohit <58105910+Mohit-8@users.noreply.github.com>
2020-08-10 01:38:52 +0530

    initial files added

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React , {useState,useEffect} from 'react';
+import axios from 'axios';
+import Header from './components/ui/header';
+import Search from './components/ui/search';
+import CharacterGrid from './components/characters/characterGrid';
+import './App.css';
+
+const App = () => {
+
+  const [items,setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query,setQuery] = useState('')
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+      console.log(result.data);
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+
+  }, [query]) //whenever query is changed useEffect is going to fire
+
+  return (
+    <div className="container">
+    <Header/>
+    <Search getQuery={(q) => setQuery(q)} />
+    <CharacterGrid isLoading={isLoading} items={items} />
+    </div>
+  );
+}
+
+export default App;
